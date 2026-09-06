1d4ac2fa071561b64216df41f9fc8a4bd9030d44
0xSunill <sunday7637@gmail.com>
2026-09-06 21:08:29 +0530

    first commit

---
+++ programs/amm/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("6gi14ywqAw7HR8kUUrRaK8dkXNdW2PyRifZT1nMkbct2");
+
+#[program]
+pub mod amm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        crate::instructions::initialize::handle_initialize(ctx)
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        crate::instructions::increment::handle_increment(ctx)
+    }
+}
