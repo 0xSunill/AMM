ed69ea34711f44068c643e76bdc55053cf7e1653
0xSunill <sunday7637@gmail.com>
2026-09-08 12:46:00 +0530

    add poolstate

--- programs/amm/src/state.rs
+++ programs/amm/src/state.rs
@@ -2,7 +2,16 @@ use anchor_lang::prelude::*;
 
 #[account]
 #[derive(InitSpace)]
-pub struct Counter {
-    pub count: u64,
-    pub authority: Pubkey,
+pub struct PoolState {
+    pub token_a_mint: Pubkey,
+    pub token_b_mint: Pubkey,
+
+    pub token_a_vault: Pubkey,
+    pub token_b_vault: Pubkey,
+
+    pub lp_token_mint: Pubkey,
+
+    pub fee_bps: u16,
+
+    pub bump: u8,
 }
